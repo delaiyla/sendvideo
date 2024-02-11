7eaecbf1ca9fc0454fc80fdf4effb9e585524ab3
delaiyla <159102841+delaiyla@users.noreply.github.com>
2024-02-11 20:15:39 +0500

    Вторая версия публикации видеопотока на сервер
    Публикация видео на сервер
    
    При первом запуске создаст два файла конфигурации: sendvideo_v2_input.txt и sendvideo_v2_output.txt
    sendvideo_v2_input.txt - адреса входящих видеопотоков
    Формат: flv rtmp://логин:пароль@адрес_сервера/канал
    sendvideo_v2_output.txt - адреса исходящих видеопотоков

---
+++ sendvideo_v2.cpp
@@ -0,0 +1,66 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+bool fileExists(const std::string& filename) {
+    std::ifstream file(filename.c_str());
+    return file.good();
+}
+
+std::vector<std::string> readLinesFromFile(const std::string& filename) {
+    std::ifstream file(filename);
+    std::vector<std::string> lines;
+
+    std::string line;
+    while (std::getline(file, line)) {
+        lines.push_back(line);
+    }
+
+    return lines;
+}
+
+int main() {
+	std::vector<std::string> outputLines;
+	std::vector<std::string> inputLines;
+	
+    if (fileExists("sendvideo_v2_output.txt")) {
+        outputLines = readLinesFromFile("sendvideo_v2_output.txt");
+    } else std::ofstream file("sendvideo_v2_output.txt");
+    
+     if (fileExists("sendvideo_v2_input.txt")) {
+        inputLines = readLinesFromFile("sendvideo_v2_input.txt");
+    } else std::ofstream file("sendvideo_v2_input.txt");
+	
+    std::ifstream inputFile("sendvideo_v2_input.txt");
+    if (!inputFile) {
+        std::cerr << "Failed to open input file" << std::endl;
+        return 1;
+    }
+
+    std::string stream;
+    std::vector<std::string>::iterator ip = outputLines.begin();
+    while (std::getline(inputFile, stream)) {
+        std::string command = "ffmpeg -hide_banner -re -fflags +genpts -stream_loop -1 -i " +
+                              stream + " -c copy -vsync 1 -tune zerolatency -pix_fmt yuv420p -f " + *ip +
+                              " -indexmem 512 -rtbufsize 256K -probesize 512 -analyzeduration 0 " +
+                              "-thread_queue_size 2048 -avoid_negative_ts \"make_zero\"";
+							  // > /dev/null 2>&1 < /dev/null &
+        if(ip != outputLines.end()) ++ip;
+
+        // Execute the ffmpeg command
+        //std::cout << command << std::endl;
+        
+ 		// Запус отдельным процессом каждого отправляемого видеопотока на сервер
+
+ #if defined(_WIN32)
+        system(("start cmd.exe @cmd /k " + command).c_str());
+ #else
+		system(("screen -d -m -S jabber " + command).c_str());
+ #endif
+    }
+
+    inputFile.close();
+
+    return 0;
+}
